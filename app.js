a34c3746d5ea0bc306e61870953d471e7cc7a4ad
ValoraSheikh <haarishsheikh04@gmail.com>
2024-10-16 09:11:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,31 +37,3 @@ function calculate() {
     }
     input.value = eval(input.value); // Be cautious with eval for security reasons
 }
-
-
-
-
-
-
-
-
-// CALCULATOR PROGRAM
-
-// const display = document.getElementById("display");
-
-// function appendToDisplay(input){
-//     display.value += input;
-// }
-
-// function clearDisplay(){
-//     display.value = "";
-// }
-
-// function calculate(){
-//     try{
-//         display.value = eval(display.value);
-//     }
-//     catch(error){
-//         display.value = "Error";
-//     }
-// }
